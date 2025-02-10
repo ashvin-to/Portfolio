e9798a6d5acdc3b2cf499fada2cf2f79f17afd75
Ashvin Singh <136104771+ashvin-to@users.noreply.github.com>
2025-02-11 01:36:01 +0530

    initial

---
+++ script.js
@@ -0,0 +1,4 @@
+// Toggle the navigation menu on mobile when hamburger icon is clicked
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
